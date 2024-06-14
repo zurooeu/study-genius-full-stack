35db70333e051110f07dbeeeb35813f36c308bf7
Zuroo <lukasz.golinski.dev@gmail.com>
2024-06-14 14:50:24 +0200

    integrated basic model completion into the flow

--- backend/app/ai/assistant.py
+++ backend/app/ai/assistant.py
@@ -1,3 +1,4 @@
+
 from fastapi import HTTPException
 from langchain_core.output_parsers import StrOutputParser
 from langchain_openai import ChatOpenAI
@@ -42,35 +43,25 @@ class LLMController:
         output = response.results[0]
         return output.flagged
 
-    def single_completion(
-        self,
-        system_input: str,
-        messages_list: list[CnvMessage],
-        model: str = OpenAIModels.gpt_35_turbo,
-        max_tokens: int = 2000,
-    ) -> str | None:
-        typed_messages: list[
-            ChatCompletionSystemMessageParam
-            | ChatCompletionUserMessageParam
-            | ChatCompletionAssistantMessageParam
-        ] = []
+    def single_completion(self,
+                          system_input: str,
+                          messages_list: list[CnvMessage],
+                          model: str = OpenAIModels.gpt_35_turbo,
+                          max_tokens: int = 2000) -> str | None:
+        typed_messages: list[ChatCompletionSystemMessageParam |
+                             ChatCompletionUserMessageParam |
+                             ChatCompletionAssistantMessageParam] = []
         for msg in messages_list:
             if msg.role == "user":
-                typed_messages.append(
-                    ChatCompletionUserMessageParam(role="user", content=msg.content)
-                )
+                typed_messages.append(ChatCompletionUserMessageParam(role="user", content=msg.content))
             elif msg.role == "assistant":
-                typed_messages.append(
-                    ChatCompletionAssistantMessageParam(
-                        role="assistant", content=msg.content
-                    )
-                )
-        system_message = ChatCompletionSystemMessageParam(
-            role="system", content=system_input
-        )
+                typed_messages.append(ChatCompletionAssistantMessageParam(role="assistant", content=msg.content))
+        system_message = ChatCompletionSystemMessageParam(role="system", content=system_input)
         typed_messages.insert(0, system_message)
         chat_completion = self.openai_client.chat.completions.create(
-            model=model, max_tokens=max_tokens, messages=typed_messages
+            model=model,
+            max_tokens=max_tokens,
+            messages=typed_messages
         )
         return chat_completion.choices[0].message.content
 
@@ -97,9 +88,7 @@ def generate_answer(*, session: Session, owner_id: int, conv_id: int) -> CnvMess
             owner_id=owner_id,
             conv_id=conversation.id,
         )
-    generated_answer = llm.single_completion(
-        system_input=SystemPrompts.assistant, messages_list=conversation.messages
-    )
+    generated_answer = llm.single_completion(system_input=SystemPrompts.assistant, messages_list=conversation.messages)
     assistant_msg = CnvMessageAssistantCreate(content=generated_answer)
     return crud.create_cnvmessage(
         session=session,
@@ -118,8 +107,6 @@ def generate_summary(*, session: Session, conv_id: int) -> str | None:
     if settings.AI_MOCK_REST_CALLS:
         return StaticAnswers.mock_summary
     llm = LLMController()
-    return llm.single_completion(
-        system_input=SystemPrompts.summary_generator,
-        messages_list=conversation.messages,
-        max_tokens=20,
-    )
+    return llm.single_completion(system_input=SystemPrompts.summary_generator,
+                                 messages_list=conversation.messages,
+                                 max_tokens=20)
